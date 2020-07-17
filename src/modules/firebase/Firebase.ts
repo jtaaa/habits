598a1d127e7921a5718f4cec420a29bed91803e2
Joshua Allum <joshuatallum@gmail.com>
2020-07-17 00:03:36 -0400

    Enable persistence

--- src/modules/firebase/Firebase.ts
+++ src/modules/firebase/Firebase.ts
@@ -27,6 +27,18 @@ class Firebase {
     if (process.env.NODE_ENV === 'production') {
       this.analytics = app.analytics();
     }
+
+    this.firestore.enablePersistence().catch(function (err) {
+      if (err.code == 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled
+        // in one tab at a a time.
+        // ...
+      } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+        // ...
+      }
+    });
   }
 }
 
